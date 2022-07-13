4f3d36d6f2e170eb33fe107d572c4ba1f56ad9d4
Winslinn <59421738+Winslinn@users.noreply.github.com>
2022-07-13 12:27:41 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,5 @@
 local player = game.Players.LocalPlayer
 local character = player.CharacterAppearanceLoaded:Wait() or player.Character
-local uis = game:GetService("UserInputService")
 
 local jumpHeight = character:WaitForChild("Humanoid").JumpHeight
 
